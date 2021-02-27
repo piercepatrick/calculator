a878f14a03abb08ab5e51828c03c0c12c4a7af65
Pierce <piercepatrick2001@gmail.com>
2021-02-27 13:55:01 -0600

    when numbers are clicked, number displays

--- app.js
+++ app.js
@@ -25,3 +25,19 @@ function operate(num1, num2, operator)
     if (operator == '*') multiply(num1,num2);
     if (operator == '/') divide(num1,num2);
 }
+
+// loop through number buttons adding event listeners to each so that when they are clicked h3 changes to that number
+
+// buttons is a node list. It looks and acts much like an array.
+const numbers = document.querySelectorAll('.numbers');
+const displayText = document.querySelector("h3");
+// we use the .forEach method to iterate through each button
+numbers.forEach((number) => {
+
+  // and for each one we add a 'click' listener
+  number.addEventListener('click', () => {
+      //displayText.append(number.innerHTMLl);
+    displayText.innerHTML = number.innerHTML;
+    let displayValue = number.innerHTML;
+  });
+});
