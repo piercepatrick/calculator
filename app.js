1b1c21034673d227a662057c2e76214a4853726c
Pierce <piercepatrick2001@gmail.com>
2021-02-27 15:03:37 -0600

    added event listener for equal sign, equal sign now displays the concatenation of strings which is next up

--- app.js
+++ app.js
@@ -20,24 +20,58 @@ function divide(a, b)
 
 function operate(num1, num2, operator)
 {
-    if (operator == '+') add(num1,num2);
-    if (operator == '-') subtract(num1,num2);
-    if (operator == '*') multiply(num1,num2);
-    if (operator == '/') divide(num1,num2);
+    if (operator == '+') return add(num1,num2);
+    if (operator == '-') return subtract(num1,num2);
+    if (operator == '*') return multiply(num1,num2);
+    if (operator == '/') return divide(num1,num2);
 }
 
-// loop through number buttons adding event listeners to each so that when they are clicked h3 changes to that number
-
+let firstValue = '';
+let operatorValue;
+let displayValue = '';
 // buttons is a node list. It looks and acts much like an array.
 const numbers = document.querySelectorAll('.numbers');
 const displayText = document.querySelector("h3");
+
 // we use the .forEach method to iterate through each button
 numbers.forEach((number) => {
-
   // and for each one we add a 'click' listener
   number.addEventListener('click', () => {
-      //displayText.append(number.innerHTMLl);
-    displayText.innerHTML = number.innerHTML;
-    let displayValue = number.innerHTML;
+    if (displayText.innerHTML.length >= 9)
+    {
+        alert('No more than 9 characters!');   
+    } 
+    else
+    {
+    displayText.innerHTML = displayText.innerHTML+number.innerHTML;
+    displayValue = displayText.innerHTML;
+    }
   });
 });
+
+
+
+const operators = document.querySelectorAll('.operators');
+
+operators.forEach((operator) => {
+    // and for each one we add a 'click' listener
+    operator.addEventListener('click', () => {
+      firstValue = displayText.innerHTML;
+      operatorValue = operator.innerHTML;
+    });
+  });
+
+
+
+const equals = document.querySelector('.equals');
+
+   
+equals.addEventListener('click', () => {
+    if (firstValue != '' && displayValue != '')
+    {
+        displayText.innerHTML = operate(firstValue, displayValue, operatorValue);
+    }
+});
+
+// next up: make strings ints
+// first value is always displayed, display second value after it is clicked and then display total after equal sign is pushed.
